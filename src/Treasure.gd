95b689982bca8ff681c05bcc82671e5ed8b27f63
Gustav <sir.gustav.the.coder@gmail.com>
2023-03-22 22:18:48 +0100

    Add treasure

---
+++ src/Treasure.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+onready var treasure_effect = preload("res://prefabs/TreasureEffect.tscn")
+
+var dx = 0
+var dy = 0
+
+const FRAC = 0.5
+const LIM = 10
+
+func _ready():
+	var treasures = ["red", "blue", "green"]
+	$Sprite.animation = treasures[randi() % treasures.size()]
+
+
+func _process(dt):
+	dy += dt * 100
+	
+	if move_and_collide(Vector2(0, dy*dt)):
+		if abs(dy) > LIM:
+			dy *= -FRAC
+		else:
+			dy = 0
+	if move_and_collide(Vector2(dx*dt, 0)):
+		dx *= -FRAC
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		queue_free()
+		var effect = treasure_effect.instance()
+		effect.set_position(position)
+		get_parent().add_child(effect)
+		body.collect_coin()

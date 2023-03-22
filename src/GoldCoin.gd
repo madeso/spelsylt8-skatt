e4a13ecc22a5d06af052772e6d26c8cd9e2c613f
Gustav <sir.gustav.the.coder@gmail.com>
2023-03-22 21:26:40 +0100

    Pickup coin effect

--- src/GoldCoin.gd
+++ src/GoldCoin.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+onready var coin_scene = preload("res://prefabs/CoinEffect.tscn")
 
 func _on_GoldCoin_body_entered(body):
 	if body.name == "Player":
 		queue_free()
+		var coin = coin_scene.instance()
+		coin.set_position(position)
+		get_parent().add_child(coin)
 		body.collect_coin()
